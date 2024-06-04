7ee654cf8e69d889fdcd8014d313ae711921502b
guimats <guilherme.mat.ds@gmail.com>
2024-06-04 10:58:33 -0300

    Finalizando aula sobre abstract factory

--- factories/abstract_factory/abstract_factory.py
+++ factories/abstract_factory/abstract_factory.py
@@ -1,3 +1,14 @@
+"""
+Abstract Factory é um padrão de criação que fornece uma interface para criar
+famílias de objetos relacionados ou dependentes sem especificar suas classes
+concretas. Geralmente Abstract Factory conta com um ou mais Factory Methods
+para criar seus objetos.
+
+Uma diferença importante entre Factory Method e Abstract Factory é que o
+Factory Method usa herança, enquanto Abstract Factory usa a composição.
+
+Princípio: programe para interfaces, não para implementações
+"""
 from abc import ABC, abstractmethod
 
 
