f16b0c076afef19b3d563ecc17a320843636de8e
guimats <guilherme.mat.ds@gmail.com>
2024-06-06 17:48:12 -0300

    Inicio iterator

---
+++ behavioral/iterator/iterator_1.py
@@ -0,0 +1,17 @@
+"""
+Iterator é um padrão comportamental que tem a
+intenção de fornecer um meio de acessar,
+sequencialmente, os elementos de um objeto
+agregado sem expor sua representação subjacente.
+
+- Uma coleção deve fornecer um meio de acessar
+    seus elementos sem expor sua estrutura interna
+- Uma coleção pode ter maneiras e percursos diferentes
+    para expor seus elementos
+- Você deve separar a complexidade dos algoritmos
+    de iteração da coleção em si
+
+A ideia principal do padrão é retirar a responsabilidade
+de acesso e percurso de uma coleção, delegando tais
+tarefas para um objeto iterador.
+"""
